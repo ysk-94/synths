af6e4ae575ec88acf0e6ed4cb2e61c8face6ac61
sakamura yosuke <yosuke_sakamura@waku-2.com>
2020-02-06 11:03:54 +0900

    demo02の音量調節

--- demo2/index.js
+++ demo2/index.js
@@ -9,17 +9,19 @@ const osc = context.createOscillator();
 osc.start = osc.start || osc.noteOn;
 osc.stop = osc.stop || osc.noteOff;
 
+osc.type = (typeof osc.type === 'string') ? 'square': 1;
+osc.frequency.value = 880;
+osc.detune.value = 100;
+
 context.createGain = context.createGain || context.createGainNode;
-
 const gain = context.createGain();
-
+gain.gain.value = 0.1;
 osc.connect(gain);
 gain.connect(context.destination);
 
-osc.start(0);
-
 document.addEventListener('click', function (){
     document.removeEventListener('click', this);
+    osc.start(0);
     context.resume();
-    window.setTimeout(() => osc.stop(0), 5000);
+    window.setTimeout(() => osc.stop(0), 3000);
 });
